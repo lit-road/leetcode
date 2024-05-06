dca2e2e84e9267b0e1150c0303f60ffc27df8526
zhanghengxin <ezreal.zhang@icewhale.org>
2024-05-06 14:40:21 +0800

    refactor: optimize the reverse function in Python solution
    The reverse function in the Python solution has been optimized to improve performance. The code now uses a more efficient algorithm to reverse the given integer. This change also includes updating the variable names and adding comments for better readability.
    
    Time complexity model: O(log|x|)
    Space complexity model: O(1)

--- 7.整数反转.py
+++ 7.整数反转.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+from python3problem7 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode.cn id=7 lang=python3
 #
@@ -7,20 +12,26 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            flag = -1
-            x = -x
-        else:
-            flag = 1
-        x = int(str(x)[::-1]) * flag
-        if x < -2**31 or x > 2**31 -1:
-            return 0
-        return x
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+        rev = 0
+        while x != 0:
+            # python3 取余 与 除数（10）符号保持一致，所以是 -123 % 10 = -13 ... 7
+            digit = x % 10
+            if x < 0 and digit > 0:
+                digit -= 10
+            x = (x - digit) // 10
+            rev = rev * 10 + digit
+            if rev < INT_MIN or rev > INT_MAX:
+                return 0
+        return rev
 # @lc code=end
 
 # @history start
 class HistorySolution:
-    def reverse(self, x: int) -> int:
+    # Time complexity model: O(1)
+    # Space complexity model: O(1)
+    def reverse1(self, x: int) -> int:
         if x < 0:
             flag = -1
             x = -x
@@ -30,5 +41,21 @@ class HistorySolution:
         if x < -2**31 or x > 2**31 -1:
             return 0
         return x
+    
+    # Time complexity model: O(log|x|)
+    def reverse2(self, x: int) -> int:
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+        rev = 0
+        while x != 0:
+            # python3 取余 与 除数（10）符号保持一致，所以是 -123 % 10 = -13 ... 7
+            digit = x % 10
+            if x < 0 and digit > 0:
+                digit -= 10
+            x = (x - digit) // 10
+            rev = rev * 10 + digit
+            if rev < INT_MIN or rev > INT_MAX:
+                return 0
+        return rev
 # @history end
 
