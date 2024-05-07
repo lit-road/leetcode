002ed92a2477031249d438c3f535ae216b089546
zhanghengxin <ezreal.zhang@icewhale.org>
2024-05-07 10:08:45 +0800

    refactor: optimize strStr function in Solution class

--- 28.找出字符串中第一个匹配项的下标.py
+++ 28.找出字符串中第一个匹配项的下标.py
@@ -12,19 +12,29 @@ from typing import *
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # l_n = len(needle)
-        # l_s = len(haystack)
-        # for i in range(l_s - l_n + 1):
-        #     index = i
-        #     j = i
-        #     k = 0
-        #     while j < l_s and k < l_n and haystack[j] == needle[k] :
-        #         j += 1
-        #         k += 1
-        #     if k == l_n:
-        #         return index
-        
-        # return -1
+        next = [0] * (len(needle) + 1)
+        res = []
+        j, i = 2, 0
+        while j <= len(needle):
+            while i > 0 and needle[j - 1] != needle[i]:
+                i = next[i]
+            if needle[j - 1] == needle[i]:
+                i += 1
+            next[j] = i
+            j += 1
+        i, j = 0, 0
+        while i < len(haystack):
+            while j > 0 and haystack[i] != needle[j]:
+                j = next[j]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                res.append(i - j + 1)
+                j = next[j]
+            i += 1
+        if len(res) > 0:
+            return res[0]
+        return -1
 
 # @lc code=end
 
