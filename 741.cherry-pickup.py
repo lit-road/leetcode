a0bc92154b6fca3aaa089601c45dd58fa0b32f86
zhanghengxin <ezreal.zhang@icewhale.org>
2024-05-10 17:12:25 +0800

    741 and lcpr

---
+++ 741.cherry-pickup.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=741 lang=python3
+# @lcpr version=30122
+#
+# [741] 摘樱桃
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [[0,1,-1],[1,0,-1],[1,1,1]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[1,1,-1],[1,-1,1],[-1,1,1]]\n
+# @lcpr case=end
+
+#
+
